b403997434d873f3a94deed8bd10899a33701418
Fabrizzio Jiglau <fabrizziojiglau2004@gmail.com>
2024-03-24 23:42:55 +0200

    /lab2

--- L2/lab2.sql
+++ L2/lab2.sql
@@ -6,4 +6,16 @@ select initcap(first_name), upper(last_name), length(first_name)
 from employees
 where upper(last_name) like 'J%' or upper(last_name) like 'M%' or upper(last_name) like '__A%'; 
 
+select employee_id, first_name, last_name, nvl(to_char(manager_id), 'Nu exista manager')
+from employees;
+
+select employee_id, first_name, last_name, nvl2(to_char(manager_id), 'Exista manager','Nu exista manager')
+from employees;
+
+--se afiseaza var1 daca exista manager, var2 daca nu exista manager
+
+select employee_id, first_name, last_name, nvl2(manager_id, 'Exista manager','Nu exista manager')
+from employees;
+
+
 
